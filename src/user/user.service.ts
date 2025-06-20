e1164976cac5b274cfdbf16d78220b07530062f1
arthursmarinho <arthursnat@gmail.com>
2025-06-20 02:23:35 -0300

    feat: update user names in UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-  names: string[] = ['dsadas'];
+  names: string[] = ['fred', 'arthur', 'theo', 'joa'];
 
   getNames() {
     return this.names;
