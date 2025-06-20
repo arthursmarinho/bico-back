91d6728778bff7d3dbd9a118ae7c406b5288a21f
arthursmarinho <arthursnat@gmail.com>
2025-06-20 02:33:47 -0300

    feat: disable credentials in CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
+    credentials: false,
   });
 
   await app.listen(3000);
